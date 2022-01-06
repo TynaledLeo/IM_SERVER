6183c1598bb7bd209236d83a7ab09904a6f5d1d3
zhangsiqi <zhangsiqi@noboauto.com>
2022-01-06 11:35:52 +0800

    init

---
+++ index.js
@@ -0,0 +1,70 @@
+// 引入依赖
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const os = require('os');
+const mysql = require('mysql');
+
+
+//数据库配置
+var connection = mysql.createConnection({
+  host     : '42.193.107.6',
+  user     : 'root',
+  password : 'root',
+  database : 'IM'
+});
+connection.connect();
+
+// 组装
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 打印服务器参数
+console.log('服务器信息：',os.hostname());
+console.log('架构：',os.arch());
+console.log('操作系统：',os.type(),os.platform(),os.release());
+console.log('负载：',os.freemem()/os.totalmem());
+
+
+
+// GET请求
+app.get('/', (req, res) => {
+	console.log(req.query);
+    if (req.query!==null) {
+        res.send("?");
+    }
+});
+
+
+
+// POST请求
+// /login登录
+app.post('/login', (req, res) => {
+    const current = new Date();
+    let username = req.body.username;
+    let password = req.body.password;
+	console.log(username+'请求登录');
+    let query = `SELECT IM_USER_PASSWORD FROM IM_USER WHERE IM_USER_NAME = '${username}'`;  
+    connection.query(query, function (error, results) {
+
+        if (error){
+            console.log(error);
+            res.status(500).send('Something broke!');
+        }
+        if (results && results.length!==0 && password === results[0].IM_USER_PASSWORD) {
+	        console.log(username+'登录成功 IN '+current);
+            res.send("OK");      
+        }else{
+            console.log(username+'登录失败 IN '+current);
+            res.send("ERROR")
+        }
+      });
+    }
+)    
+
+
+
+
+let server = app.listen(8642, function () {
+  const port = server.address().port
+  console.log(">_服务已经在%s端口开启", port)
+})
